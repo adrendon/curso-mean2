443e351c287b6c684e6a2a34fbdec208c53f221c
Alexis Rendón <arendon@chefcompany.co>
2017-02-15 10:13:47 -0500

    artist controller, routes, uploads

--- controllers/artist.js
+++ controllers/artist.js
@@ -90,8 +90,94 @@ function updateArtist(req, res){
     });
 }
 
+function deleteArtist(req, res){
+    var artistId = req.params.id;
+
+    Artist.findByIdAndRemove(artistId, (err, artistRemoved) => {
+        if(err){
+            res.status(500).send({message: 'Error al guardar el artista'});
+        }else{
+            if(!artistRemoved){
+                res.status(404).send({message: 'El artista no ha sido eliminado'});
+            }else{
+                Album.find({artist: artistRemoved._id}).remove((err, albumRemoved) => {
+                    if(err){
+                         res.status(500).send({message: 'Error al eliminar el album'});
+                    }else{
+                        if(!albumRemoved){
+                            res.status(404).send({message: 'El album no ha sido eliminado'});
+                        }else{
+
+                            Song.find({album: albumRemoved._id}).remove((err, songRemoved) => {
+                                if(err){
+                                     res.status(500).send({message: 'Error al eliminar la canción'});
+                                }else{
+                                    if(!songRemoved){
+                                        res.status(404).send({message: 'La canción no ha sido eliminada'});
+                                    }else{
+                                        res.status(200).send({artist: artistRemoved});
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
+
+            }
+        }
+    })
+}
+
+function UploadImage(req, res){
+    var artistId = req.params.id;
+    var file_name= 'No subido...';
+
+    if(req.files){
+        var file_path = req.files.image.path;
+        var file_split = file_path.split('\\');
+        var file_name = file_split[2];
+
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1];
+
+        if(file_ext == 'png' || file_ext == 'jpg' || file_ext == 'gif') {
+
+            Artist.findByIdAndUpdate(artistId, {image: file_name}, (err, artistUpdated) => {
+                if(!artistUpdated){
+                    res.status(404).send({message: 'No se ha podido actualizar el usuario'});
+                }else{
+                    res.status(200).send({artist: artistUpdated});
+                }
+            });
+
+        }else {
+            res.status(200).send({message: 'Extensión del archivo no valida'});
+        }
+
+    }else{
+        res.status(200).send({message: 'No ha subido nunguna imagen'});
+    }
+}
+
+function getImageFile(req, res) {
+    var imageFile = req.params.imageFile;
+    var path_file = './uploads/users/'+imageFile;
+    fs.exists(path_file, function(exists){
+        if(exists){
+            res.sendFile(path.resolve(path_file));
+        }else{
+            res.status(200).send({message: 'No existe la imagen'});
+        }
+    });
+}
+
+
 module.exports = {
     getArtist,
     saveArtist,
-    getArtists
+    getArtists,
+    updateArtist,
+    deleteArtist,
+    UploadImage,
+    getImageFile
 };
--- services/jwt.js
+++ services/jwt.js
@@ -2,7 +2,7 @@
 
 var jwt = require('jwt-simple');
 var moment =  require('moment');
-var secret = 'clave_secreta_curo';
+var secret = 'clave_secreta_curso';
 
 exports.createToken = function (user) {
     var payload = {
